f1ac8d411ae19ee530af2b53abe20fad7d79827d
William Marzella <automation@motis.group>
2024-05-23 09:49:42 +0100

    edits

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
 import ask from './routes/ask';
 
 const app = new Hono();
+app.use(logger());
 
 app.get('/', (c) => {
 	return c.json({ message: 'Hello World' });
