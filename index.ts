1a4347ced34eb581818ae8192e307b4b415affb2
William Marzella <automation@motis.group>
2024-05-23 11:03:39 +0100

    implemented bearer auth

--- index.ts
+++ index.ts
@@ -1,13 +1,46 @@
-import { Hono } from 'hono';
+import { Hono, Context, Next } from 'hono';
 import { logger } from 'hono/logger';
 import ask from './routes/ask';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+	process.env.SUPABASE_URL,
+	process.env.SUPABASE_KEY
+);
 
 const app = new Hono();
 app.use(logger());
 
-app.get('/', (c) => {
-	return c.json({ message: 'Hello World' });
-});
+const verifyToken = async (c: Context, next: Next) => {
+	const token = c.req.header('Authorization')?.replace('Bearer ', '');
+	console.log(token);
+	if (!token) {
+		return c.json({ error: 'Unauthorized' }, 401);
+	}
+	const { data, error } = await supabase
+		.from('tokens')
+		.select('user_id, api_call_count')
+		.eq('token', token)
+		.single();
+	if (error || !data) {
+		return c.json({ error: 'Unauthorized' }, 401);
+	}
+
+	// Update api_call_count
+	await supabase
+		.from('tokens')
+		.update({ api_call_count: data.api_call_count + 1 })
+		.eq('token', token);
+
+	c.set('user', data);
+	await next();
+};
+
+app.use('/*', verifyToken);
+
+// app.get('/', (c) => {
+// 	return c.json({ message: 'Hello World' });
+// });
 
 app.route('/ask', ask);
 
